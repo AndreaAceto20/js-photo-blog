89f185c0f9eb3ab68d305be92d03c6b5f35dab84
Andrea Aceto <andrea.aceto20@gmail.com>
2025-01-24 22:15:50 +0100

    [ADD] Comments

--- js/myScrypt.js
+++ js/myScrypt.js
@@ -1,15 +1,17 @@
+// INSERISCO L'URL DELL' API IN UNA VARIABILE
 const endpoint = "https://lanciweb.github.io/demo/api/pictures/"
-
-
-
+// PRENDO I DATI DALL'API
 axios.get(endpoint)
 .then(responseObj =>{
+    //SALVO LA RISPOSTA DELL'API 
     let debug = responseObj;
+        // PRENDO LA PARTE DEI DATI
     let data = responseObj.data;
+    // STAMPO IN CONSOLE PER DEBUG
     console.log(data);
-    
+    // CREO VARIABILE PER POI AGGIUNGERE LE SINGOLE CARD
     let cards = document.getElementById("cards");
-
+    // CICLO PER INSERIRE NELLA CARD I VALORI CHE PRENDO DALL'API
     for( i = 0 ; i < data.length ; i++){
         cards.innerHTML += 
         `
@@ -21,9 +23,8 @@ axios.get(endpoint)
                  </div>
         `
     }
-    
-
 })
+// SE CI SONO ERRORI LI STAMPO IN CONSOLE
 .catch(error=>{
     console.log(error);
     
