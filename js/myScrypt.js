e8b499598931e4280e515e5874b49d01af4118f2
Andrea Aceto <andrea.aceto20@gmail.com>
2025-01-24 11:46:08 +0100

    [FIXED] Problem with length of "for" cicle

--- js/myScrypt.js
+++ js/myScrypt.js
@@ -5,15 +5,12 @@ const endpoint = "https://lanciweb.github.io/demo/api/pictures/"
 axios.get(endpoint)
 .then(responseObj =>{
     let debug = responseObj;
-    var data = responseObj.data;
-    var lenght = data.lenght;
-    console.log(lenght);
-    
-    console.log(data[1].url);
+    let data = responseObj.data;
+    console.log(data);
     
     let cards = document.getElementById("cards");
 
-    for( i = 0 ; i < 6 ; i++){
+    for( i = 0 ; i < data.length ; i++){
         cards.innerHTML += 
         `
         <div class="container bg-white col-4 card">
